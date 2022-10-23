df86ebee8ee0a8a8ef53d46f00b1e2ec7d733d80
Miguel Karlo Sese <sese.miguel9@gmail.com>
2022-10-23 13:50:23 +0800

    Update index.js
    Added UL to navbar. Added page variable to put everything together.

--- react-render-components/src/index.js
+++ react-render-components/src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Navbar(){
-  return(<h1 className='navbar'>This is my navbar area.</h1>)
+  return (
+    <nav>
+      <h1 className='navbar'>This is my navbar area.</h1>
+      <ul className='links'>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
 }
 
 function MainContent(){
   return(<h1 className='main-content'>This is my main content area.</h1>)
 }
 
+const page = (
+  <div>
+    <Navbar /> 
+    <MainContent />
+  </div>
+
+)
+
 ReactDOM.render(
-  <div><Navbar /> <MainContent /></div>,
+  page,
   document.getElementById("root")
 );
